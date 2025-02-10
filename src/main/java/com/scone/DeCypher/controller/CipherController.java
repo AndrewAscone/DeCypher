cfe5f4d212b3ef12f153f8634c5fd21986ef88aa
scone527 <andrew.ascone@gmail.com>
2025-02-10 16:58:51 -0500

    Fixed annotations on service and controller classes

--- src/main/java/com/scone/DeCypher/controller/CipherController.java
+++ src/main/java/com/scone/DeCypher/controller/CipherController.java
@@ -6,7 +6,11 @@ import com.scone.DeCypher.service.CipherService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class CipherController {
     private final CipherService cipherService;
 
--- src/main/java/com/scone/DeCypher/service/CipherService.java
+++ src/main/java/com/scone/DeCypher/service/CipherService.java
@@ -1,5 +1,8 @@
 package com.scone.DeCypher.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CipherService {
 
     public String encrypt(String text, String cipher){
