d36ccf7687c76d4615a1e34273a9889150abea7c
scone527 <andrew.ascone@gmail.com>
2025-02-10 16:42:55 -0500

    Added placeholder controller class

---
+++ src/main/java/com/scone/DeCypher/controller/CipherController.java
@@ -0,0 +1,28 @@
+package com.scone.DeCypher.controller;
+
+import com.scone.DeCypher.model.EncryptionRequest;
+import com.scone.DeCypher.model.EncryptionResponse;
+import com.scone.DeCypher.service.CipherService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class CipherController {
+    private final CipherService cipherService;
+
+    public CipherController(CipherService cipherService){
+        this.cipherService = cipherService;
+    }
+
+    @PostMapping("/encrypt")
+    public ResponseEntity<EncryptionResponse> encrypt(@RequestBody EncryptionRequest request){
+        String result = cipherService.encrypt(request.getText(), request.getCipher());
+        return ResponseEntity.ok(new EncryptionResponse(result, request.getCipher()));
+    }
+
+    @PostMapping("/decrypt")
+    public ResponseEntity<EncryptionResponse> decrypt(@RequestBody EncryptionRequest request){
+        String result = cipherService.decrypt(request.getText(), request.getCipher());
+        return ResponseEntity.ok(new EncryptionResponse(result, request.getCipher()));
+    }
+}
