f57775eac057f2fc06c8bc7baec2f416d7421181
scone527 <andrew.ascone@gmail.com>
2025-02-10 16:16:14 -0500

    Added placeholder service class

---
+++ src/main/java/com/scone/DeCypher/service/CipherService.java
@@ -0,0 +1,14 @@
+package com.scone.DeCypher.service;
+
+public class CipherService {
+
+    public String encrypt(String text, String cipher){
+        //Placeholder logic
+        return new StringBuilder(text).reverse().toString();
+    }
+
+    public String decrypt(String text, String cipher){
+        //Placeholder logic
+        return new StringBuilder(text).reverse().toString();
+    }
+}
