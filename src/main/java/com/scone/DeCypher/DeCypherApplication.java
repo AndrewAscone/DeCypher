2603cc310c4c12fc2c7ead9690dcfe012b1ded6c
scone527 <andrew.ascone@gmail.com>
2025-02-10 16:02:44 -0500

    First commit

---
+++ src/main/java/com/scone/DeCypher/DeCypherApplication.java
@@ -0,0 +1,13 @@
+package com.scone.DeCypher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeCypherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeCypherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scone/DeCypher/DeCypherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scone.DeCypher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeCypherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
