08405d75c670d94edddb14fab744f46a2f7072e0
scone527 <andrew.ascone@gmail.com>
2025-02-10 16:13:21 -0500

    Created EncryptionResponse model

---
+++ src/main/java/com/scone/DeCypher/model/EncryptionResponse.java
@@ -0,0 +1,31 @@
+package com.scone.DeCypher.model;
+
+public class EncryptionResponse {
+    private String result;
+    private String cipher;
+
+    public EncryptionResponse(){
+
+    }
+
+    public EncryptionResponse(String result, String cipher){
+        this.result = result;
+        this.cipher = cipher;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getCipher() {
+        return cipher;
+    }
+
+    public void setCipher(String cipher) {
+        this.cipher = cipher;
+    }
+}
