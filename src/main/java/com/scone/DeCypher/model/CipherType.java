112c07bf5540b318bce78854b1e2b72741c79a8a
scone527 <andrew.ascone@gmail.com>
2025-02-11 18:31:09 -0500

    Creating cipher enum to simplify logic in service class

---
+++ src/main/java/com/scone/DeCypher/model/CipherType.java
@@ -0,0 +1,47 @@
+package com.scone.DeCypher.model;
+
+import java.util.function.BiFunction;
+
+public enum CipherType {
+    CAESAR(CipherType::caesarEncrypt, CipherType::caesarDecrypt),
+    VIGENERE(CipherType::vigenereEncrypt, CipherType::vigenereDecrypt);
+
+    private final BiFunction<String, Integer, String> encryptFunction;
+    private final BiFunction<String, Integer, String> decryptFunction;
+
+    CipherType(BiFunction<String, Integer, String> encryptFunction, BiFunction<String, Integer, String> decryptFunction){
+        this.encryptFunction = encryptFunction;
+        this.decryptFunction = decryptFunction;
+    }
+
+    public String encrypt(String text, int shiftOrKey){
+        return encryptFunction.apply(text, shiftOrKey);
+    }
+
+    public String decrypt(String text, int shiftOrKey){
+        return decryptFunction.apply(text, shiftOrKey);
+    }
+
+    //Caesar cipher encrypt/decrypt
+    private static String caesarEncrypt(String text, int shift){
+        StringBuilder result = new StringBuilder();
+        shift = shift % 26;
+
+        for(char ch : text.toCharArray()){
+            if(Character.isLetter(ch)){
+                char base = Character.isUpperCase(ch) ? 'A' : 'a';
+                result.append((char) ((ch - base + shift) % 26 + base));
+            } else {
+                result.append(ch);
+            }
+        }
+
+        return result.toString();
+    }
+
+    private static String caesarDecrypt(String text, int shift){
+        return caesarEncrypt(text, 26 - (shift % 26));
+    }
+
+    //Vigenere cipher encrypt/decrypt
+}
