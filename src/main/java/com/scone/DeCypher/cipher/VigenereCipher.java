0db04acc2a228ebd4e391cc8d63ee3a327b19f56
scone527 <andrew.ascone@gmail.com>
2025-02-13 19:46:26 -0500

    Created VigenereCipher class

---
+++ src/main/java/com/scone/DeCypher/cipher/VigenereCipher.java
@@ -0,0 +1,18 @@
+package com.scone.DeCypher.cipher;
+
+public class VigenereCipher implements Cipher{
+    @Override
+    public String encrypt(String text, String key) {
+        return "";
+    }
+
+    @Override
+    public String decrypt(String text, String key) {
+        return "";
+    }
+
+    @Override
+    public boolean isValidKey(String key) {
+        return false;
+    }
+}
