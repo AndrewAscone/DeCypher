f3b1e4f02f183394d35b062118eb9d15bdfe31bb
scone527 <andrew.ascone@gmail.com>
2025-02-14 19:17:17 -0500

    Working on vigenere cipher class

--- src/main/java/com/scone/DeCypher/cipher/VigenereCipher.java
+++ src/main/java/com/scone/DeCypher/cipher/VigenereCipher.java
@@ -13,6 +13,24 @@ public class VigenereCipher implements Cipher{
 
     @Override
     public boolean isValidKey(String key) {
-        return false;
+        return key != null && key.matches("[A-Za-z]+"); // Regex here means 'Only letters, at least one character'
+    }
+
+    private String formatKey(String text, String key){
+        StringBuilder formattedKey = new StringBuilder();
+        int keyLength = key.length();
+        int textLength = text.length();
+
+        for (int i = 0, j = 0; i < textLength; i++) {
+            char nextChar = text.charAt(i);
+            if(Character.isLetter(nextChar)){
+                formattedKey.append(key.charAt(j % keyLength));
+                j++;
+            } else {
+                formattedKey.append(nextChar);
+            }
+        }
+
+        return formattedKey.toString();
     }
 }
