9bba276436d9d585f5249c275afe56fde1d6f588
scone527 <andrew.ascone@gmail.com>
2025-02-24 14:41:14 -0500

    Updated service class to reflect refactoring changes

--- src/main/java/com/scone/DeCypher/service/CipherService.java
+++ src/main/java/com/scone/DeCypher/service/CipherService.java
@@ -1,5 +1,6 @@
 package com.scone.DeCypher.service;
 
+import com.scone.DeCypher.cipher.CipherFactory;
 import com.scone.DeCypher.model.CipherType;
 import org.springframework.stereotype.Service;
 
@@ -8,21 +9,17 @@ import javax.crypto.Cipher;
 @Service
 public class CipherService {
 
-    public String encrypt(String text, String cipher, int shift, String key){
-        try {
-            CipherType cipherType = CipherType.valueOf(cipher.toUpperCase());
-            return cipherType.encrypt(text, key);
-        } catch (IllegalArgumentException e){
-                throw new IllegalArgumentException("Unsupported cipher type: " + cipher);
-        }
+    private final CipherFactory cipherFactory;
+
+    public CipherService(CipherFactory cipherFactory){
+        this.cipherFactory = cipherFactory;
     }
 
-    public String decrypt(String text, String cipher, int shift, String key){
-        try{
-            CipherType cipherType = CipherType.valueOf(cipher.toUpperCase());
-            return cipherType.decrypt(text, key);
-        } catch (IllegalArgumentException e) {
-            throw new RuntimeException("Unsupported cipher type: " + cipher);
-        }
+    public String encrypt(String text, String cipher, String key){
+        return cipherFactory.getCipher(cipher).encrypt(text, key);
+    }
+
+    public String decrypt(String text, String cipher, String key){
+        return cipherFactory.getCipher(cipher).decrypt(text, key);
     }
 }
