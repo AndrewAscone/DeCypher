586816c04f9aeaa26d8e457ff0dd2176dd105486
scone527 <andrew.ascone@gmail.com>
2025-02-27 16:15:54 -0500

    Finished filling out AES cipher functions

--- src/main/java/com/scone/DeCypher/cipher/AESCipher.java
+++ src/main/java/com/scone/DeCypher/cipher/AESCipher.java
@@ -1,7 +1,11 @@
 package com.scone.DeCypher.cipher;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 public class AESCipher implements EncryptionCipher {
     private final byte[] key;
@@ -14,17 +18,17 @@ public class AESCipher implements EncryptionCipher {
 
     @Override
     public String encrypt(String text) {
-        return "";
+        return processCipher(text, Cipher.ENCRYPT_MODE);
     }
 
     @Override
     public String decrypt(String text) {
-        return "";
+        return processCipher(text, Cipher.DECRYPT_MODE);
     }
 
     @Override
     public boolean isValidKey() {
-        return false;
+        return key.length == 16; // AES-128 requires a 16-byte key
     }
 
     private byte[] generateIV(){
@@ -44,7 +48,19 @@ public class AESCipher implements EncryptionCipher {
 
     private String processCipher(String text, int mode){
         try{
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+            cipher.init(mode, secretKeySpec, new IvParameterSpec(iv));
 
+            byte[] outputBytes = (mode == Cipher.ENCRYPT_MODE) ?
+                    cipher.doFinal(text.getBytes(StandardCharsets.UTF_8)) :
+                    cipher.doFinal(Base64.getDecoder().decode(text));
+
+            return (mode == Cipher.ENCRYPT_MODE) ?
+                    Base64.getEncoder().encodeToString(outputBytes) :
+                    new String(outputBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException("AES processing failed", e);
         }
     }
 }
