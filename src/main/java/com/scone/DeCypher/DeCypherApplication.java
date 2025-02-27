0549356569b5731d872f567acde01eb3aaf362f7
scone527 <andrew.ascone@gmail.com>
2025-02-26 19:41:06 -0500

    Registering Bouncy Castle as security provider in psvm

--- src/main/java/com/scone/DeCypher/DeCypherApplication.java
+++ src/main/java/com/scone/DeCypher/DeCypherApplication.java
@@ -3,10 +3,14 @@ package com.scone.DeCypher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 @SpringBootApplication
 public class DeCypherApplication {
 
 	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(DeCypherApplication.class, args);
 	}
 
