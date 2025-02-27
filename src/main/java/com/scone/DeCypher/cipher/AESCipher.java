6f02dceb9f9f9d34cbe0185c8c2af59e1efb4a4d
scone527 <andrew.ascone@gmail.com>
2025-02-27 15:37:39 -0500

    Reworking plan for AES cipher, some Bouncy Castle functions deprecated

--- src/main/java/com/scone/DeCypher/cipher/AESCipher.java
+++ src/main/java/com/scone/DeCypher/cipher/AESCipher.java
@@ -1,5 +1,9 @@
 package com.scone.DeCypher.cipher;
 
+import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 
@@ -28,18 +32,18 @@ public class AESCipher implements Cipher{
     }
 
     private byte[] generateIV(){
-        byte[] iv = new byte[16]; // AES block size is 16 bytes
+        byte[] iv = new byte[16]; // CBC mode uses a 16-byte IV
         new SecureRandom().nextBytes(iv);
         return iv;
     }
 
     private byte[] validateAndFormatKey(String keyString){
         byte[] keyBytes = keyString.getBytes(StandardCharsets.UTF_8);
-        if(keyBytes.length == 16 || keyBytes.length == 24 || keyBytes.length == 32){
+        if(keyBytes.length == 16){
             return keyBytes;
         }
 
-        throw new IllegalArgumentException("AES key must be 16, 24, or 32 bytes.");
+        throw new IllegalArgumentException("AES key must be exactly 16 bytes (128-bit).");
     }
 
     private byte[] processData(byte[] input, boolean encrypt){
