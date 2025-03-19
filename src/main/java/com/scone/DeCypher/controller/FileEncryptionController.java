53d9799d35da4dd34a10ed66c96a63f35a541d16
AndrewAscone <andrew.ascone@gmail.com>
2025-03-19 18:50:14 -0400

    Setting up structure for file encryption controller

--- src/main/java/com/scone/DeCypher/controller/FileEncryptionController.java
+++ src/main/java/com/scone/DeCypher/controller/FileEncryptionController.java
@@ -1,4 +1,28 @@
 package com.scone.DeCypher.controller;
 
+import com.scone.DeCypher.service.FileEncryptionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
 public class FileEncryptionController {
+    private final FileEncryptionService fileEncryptionService;
+
+    public FileEncryptionController (FileEncryptionService fileEncryptionService){
+        this.fileEncryptionService = fileEncryptionService;
+    }
+
+    public ResponseEntity<byte[]> encryptFile(){
+        return null;
+    }
+
+    public ResponseEntity<byte[]> decryptFile(){
+        return null;
+    }
+
+    private ResponseEntity<byte[]> processFile(){
+        return null;
+    }
 }
