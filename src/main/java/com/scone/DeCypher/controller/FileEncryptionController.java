dc0807e708802f4e0969bf8f3df3db94d1a06cae
AndrewAscone <andrew.ascone@gmail.com>
2025-03-19 19:06:14 -0400

    Filling out methods in file encryption controller

--- src/main/java/com/scone/DeCypher/controller/FileEncryptionController.java
+++ src/main/java/com/scone/DeCypher/controller/FileEncryptionController.java
@@ -3,7 +3,9 @@ package com.scone.DeCypher.controller;
 import com.scone.DeCypher.service.FileEncryptionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/file")
@@ -14,15 +16,19 @@ public class FileEncryptionController {
         this.fileEncryptionService = fileEncryptionService;
     }
 
-    public ResponseEntity<byte[]> encryptFile(){
-        return null;
+    public ResponseEntity<byte[]> encryptFile(@RequestParam("file") MultipartFile file,
+                                              @RequestParam("cipher") String cipherName,
+                                              @RequestParam("key") String key){
+        return processFile(file, cipherName, key, true);
     }
 
-    public ResponseEntity<byte[]> decryptFile(){
-        return null;
+    public ResponseEntity<byte[]> decryptFile(@RequestParam("file") MultipartFile file,
+                                              @RequestParam("cipher") String cipherName,
+                                              @RequestParam("key") String key){
+        return processFile(file, cipherName, key, false);
     }
 
-    private ResponseEntity<byte[]> processFile(){
+    private ResponseEntity<byte[]> processFile(MultipartFile file, String cipherName, String key, boolean encrypt){
         return null;
     }
 }
