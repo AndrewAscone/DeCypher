00d3dc445bcf0b47ca3dfcddf284007380f5fa0a
AndrewAscone <andrew.ascone@gmail.com>
2025-04-08 17:53:47 -0400

    Making CORS access restricted to necessary paths and methods

--- src/main/java/com/scone/DeCypher/backend/WebConfig.java
+++ src/main/java/com/scone/DeCypher/backend/WebConfig.java
@@ -13,9 +13,14 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("POST")
+                        .allowedHeaders("*");
+
+                registry.addMapping("/file/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("POST")
                         .allowedHeaders("*");
             }
         };
