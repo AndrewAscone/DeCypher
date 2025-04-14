9057c0cea19b4a91ce6b7097d5f37c9a5f516539
AndrewAscone <andrew.ascone@gmail.com>
2025-04-14 18:47:58 -0400

    Rewriting WebConfig to accommodate environment flag setup

--- src/main/java/com/scone/DeCypher/backend/WebConfig.java
+++ src/main/java/com/scone/DeCypher/backend/WebConfig.java
@@ -1,28 +1,19 @@
 package com.scone.DeCypher.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("POST")
-                        .allowedHeaders("*");
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
-                registry.addMapping("/file/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("POST")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+
     }
 }
