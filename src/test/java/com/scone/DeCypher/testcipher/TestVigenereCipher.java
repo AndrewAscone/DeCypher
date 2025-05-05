6b58cd07aeba2934e92f99f7ae81e857538ea37a
AndrewAscone <andrew.ascone@gmail.com>
2025-05-05 18:59:44 -0400

    Writing Vigenere cipher tests

--- src/test/java/com/scone/DeCypher/testcipher/TestVigenereCipher.java
+++ src/test/java/com/scone/DeCypher/testcipher/TestVigenereCipher.java
@@ -1,4 +1,54 @@
 package com.scone.DeCypher.testcipher;
 
+import com.scone.DeCypher.backend.cipher.VigenereCipher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestVigenereCipher {
+
+    private VigenereCipher cipher;
+    private final String defaultKey = "key";
+
+    @BeforeEach
+    void setUp() {
+        cipher = new VigenereCipher(defaultKey);
+    }
+
+    @Test
+    void testEncryptText() {
+        // given
+        String input = "HELLOworld";
+
+        // when
+        String encrypted = cipher.encrypt(input);
+
+        // then
+        assertEquals("RIJVSuyvjn", encrypted);
+    }
+
+    @Test
+    void testDecryptText() {
+        // given
+        String encrypted = cipher.encrypt("HELLOworld");
+
+        // when
+        String decrypted = cipher.decrypt(encrypted);
+
+        // then
+        assertEquals("HELLOworld", decrypted);
+    }
+
+    @Test
+    void testInputWithNumericCharacters() {
+        // given
+        String input = "Hello, World! 123";
+
+        // when
+        String encrypted = cipher.encrypt(input);
+        String decrypted = cipher.decrypt(encrypted);
+
+        // then
+        assertEquals(input, decrypted);
+    }
 }
