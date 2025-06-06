06bba953e42d959181209f2a3c5d4e73a954e3a0
AndrewAscone <andrew.ascone@gmail.com>
2025-06-06 19:22:19 -0400

    Setting up chained request class

--- src/main/java/com/scone/DeCypher/backend/model/ChainedCipherRequest.java
+++ src/main/java/com/scone/DeCypher/backend/model/ChainedCipherRequest.java
@@ -1,4 +1,38 @@
 package com.scone.DeCypher.backend.model;
 
+import java.util.List;
+
 public class ChainedCipherRequest {
+    private String text;
+    private List<CipherStep> steps;
+
+    public static class CipherStep {
+        private String cipher;
+        private String key;
+
+        public CipherStep() {
+
+        }
+
+        public CipherStep(String cipher, String key) {
+            this.cipher = cipher;
+            this.key = key;
+        }
+
+        public String getCipher() {
+            return cipher;
+        }
+
+        public void setCipher(String cipher) {
+            this.cipher = cipher;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+    }
 }
